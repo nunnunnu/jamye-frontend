86026bea263fe62b77497f7d3337a2bf26485274
박진희 <110175918+nunnunnu@users.noreply.github.com>
2025-01-23 22:44:03 +0900

    토큰 재발급 구조 생성

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+// import VueCookie from 'vue-cookie';
 
 let activeRequests = 0;
 let loadingCallback = null;
@@ -26,12 +27,31 @@ instance.interceptors.response.use((response) => {
 }, async (error) => {
     activeRequests--;
     if (error.response && error.response.status === 403) {
-      
+      const refreshToken = document.cookie.match('(^|;) ?' + 'refreshToken' + '=([^;]*)(;|$)')[2];
+      console.log(refreshToken)
       try {
-        // const refreshResponse = await instance.post('/auth/refresh-token');
-        // console.log('토큰 리프레시 성공:', refreshResponse.data);
+        const refreshResponse = await instance.post('/api/user/refresh', {
+          refreshToken: refreshToken
+        });
+        var date = new Date();
+        date.setDate(date.getDate() + 1000 * 60 * 60 * 24);
+        console.log(date.toUTCString())
+        console.log(encodeURIComponent("accessToken") + '=' + encodeURIComponent(refreshResponse.data.accessToken) + ';expires=' + date.toUTCString() + ';path=/')
+        document.cookie = encodeURIComponent("accessToken") + '=' + encodeURIComponent(refreshResponse.data.accessToken) + ';expires=' + date.toUTCString() + ';path=/';
+        
+        console.log('토큰 리프레시 성공:', refreshResponse.data);
+        error.config.headers['Authorization'] = `Beare ${refreshResponse.data.accessToken}`
+        return instance.request(error.config)
       } catch (refreshError) {
         console.error('리프레시 API 호출 실패:', refreshError);
+        
+        // document.cookie = encodeURIComponent("accessToken") + '=; expires=Thu, 01 JAN 1999 00:00:10 GMT';
+        // document.cookie = encodeURIComponent("refreshToken") + '=; expires=Thu, 01 JAN 1999 00:00:10 GMT';
+        // document.cookie = encodeURIComponent("id") + '=; expires=Thu, 01 JAN 1999 00:00:10 GMT';
+        // document.cookie = encodeURIComponent("sequence") + '=; expires=Thu, 01 JAN 1999 00:00:10 GMT';
+        // document.cookie = encodeURIComponent("group") + '=; expires=Thu, 01 JAN 1999 00:00:10 GMT';
+
+        // VueCookie.$router.push("/login")
       }
     }
     if (activeRequests === 0 && loadingCallback) loadingCallback(false); 
