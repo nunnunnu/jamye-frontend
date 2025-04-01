822b00b5a391910b6f9214a493a04c8882297e98
박진희 <110175918+nunnunnu@users.noreply.github.com>
2025-04-01 13:21:05 +0900

    backend domain 등록

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 // config.js
-export const BASE_URL = "http://13.124.129.247:8080";
+export const BASE_URL = "https://jamye-backend.o-r.kr";
 export const redirect_uri = "http://jamye-frontend.s3-website.ap-northeast-2.amazonaws.com/oauth/redirect";
 
 // export const BASE_URL = "http://localhost:8080";
