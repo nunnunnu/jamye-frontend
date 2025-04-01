01063a0cd5aea911450fc8d9deb719062a88af06
박진희 <110175918+nunnunnu@users.noreply.github.com>
2025-04-01 13:25:41 +0900

    discord redirect url 변경

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 // config.js
 export const BASE_URL = "https://jamye-backend.o-r.kr";
-export const redirect_uri = "http://jamye-frontend.s3-website.ap-northeast-2.amazonaws.com/oauth/redirect";
+export const redirect_uri = "https://jamye.p-e.kr/oauth/redirect";
 
 // export const BASE_URL = "http://localhost:8080";
 // export const redirect_uri = "http://localhost:8081/oauth/redirect";
