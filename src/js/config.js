4d8c8c21e892424a24d23afc19cb7da6ae495432
박진희 <110175918+nunnunnu@users.noreply.github.com>
2025-04-28 22:00:22 +0900

    temp

--- src/js/config.js
+++ src/js/config.js
@@ -3,7 +3,7 @@ export const BASE_URL = "https://jamye-backend.o-r.kr";
 export const redirect_uri = "https://jamye.p-e.kr/oauth/redirect";
 export const IMAGE_URL = "https://d23eha8a4ssn72.cloudfront.net";
 export const redirectBaseUrl= "https://jamye.p-e.kr";
-
+ 
 // export const BASE_URL = "http://localhost:8080";
 // export const redirect_uri = "http://localhost:8081/oauth/redirect";
 // export const redirectBaseUrl= "http://localhost:8081";
